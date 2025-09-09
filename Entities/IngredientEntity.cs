0bec3ac3a10fee2a6b2c679d589733d0eca9cc4c
Rsda23 <rsda23.0@gmail.com>
2025-09-09 17:05:44 +0200

    feat: add ingredient entity

---
+++ Entities/IngredientEntity.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PlanMyMeal.Api.Entities
+{
+    public class IngredientEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string IngredientId { get; set; }
+
+        [BsonElement]
+        public string RecipeId { get; set; }
+
+        [BsonElement]
+        public string UserId { get; set; }
+
+        [BsonElement]
+        public string Title { get; set; }
+
+        public IngredientEntity()
+        {
+
+        }
+
+        public IngredientEntity(string recipeId, string userId, string title)
+        {
+            RecipeId = recipeId;
+            UserId = userId;
+            Title = title;
+        }
+    }
+}
