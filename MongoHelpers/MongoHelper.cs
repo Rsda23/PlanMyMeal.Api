bfca9276523c5a2c98e67663fa9d0caf2ec8aede
Rsda23 <rsda23.0@gmail.com>
2025-09-09 15:59:55 +0200

    feat: add mongo helper for database acces

---
+++ MongoHelpers/MongoHelper.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+
+namespace PlanMyMeal.Api.MongoHelpers
+{
+    public class MongoHelper
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase _database;
+        public MongoHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var connectionString = _configuration["PlanMyMealDatabase:ConnectionString"];
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception("Error lors de la connection à MongoDB");
+            }
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase("PlanMyMeal");
+        }
+        public IMongoDatabase GetDatabase()
+        {
+            return _database;
+        }
+    }
+}
