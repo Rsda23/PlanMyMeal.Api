ffeafbbf31e4fb9419eadd5a577d9c944492bfaa
Rsda23 <rsda23.0@gmail.com>
2025-09-11 17:59:53 +0200

    feat(program): add user service and mongo helper as transient services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using PlanMyMeal.Api.Interface;
+using PlanMyMeal.Api.MongoHelpers;
+using PlanMyMeal.Api.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<MongoHelper>();
 
 var app = builder.Build();
 
