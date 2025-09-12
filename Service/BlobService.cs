9cb80b0564d029614ddb982a55eb9f6afb3604e4
Rsda23 <rsda23.0@gmail.com>
2025-09-12 17:42:15 +0200

    feat: add blob service and interface

---
+++ Service/BlobService.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using PlanMyMeal.Api.Interface;
+
+namespace PlanMyMeal.Api.Service
+{
+    public class BlobService : IBlobService
+    {
+        private readonly BlobServiceClient _blobService;
+        public BlobService(IConfiguration configuration)
+        {
+            _blobService = new BlobServiceClient(configuration["AzureBlob:ConnectionString"]);
+        }
+    }
+
+}
