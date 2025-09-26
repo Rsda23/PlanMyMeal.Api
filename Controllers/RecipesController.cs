46f3feab74f48c7678805822aecc7b2d56c42a8b
Rsda23 <rsda23.0@gmail.com>
2025-09-26 12:13:04 +0200

    feat(recipe): add controller

---
+++ Controllers/RecipesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlanMyMeal.Api.Interface;
+
+namespace PlanMyMeal.Api.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly IRecipeService _recipeService;
+
+        public RecipesController(IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+    }
+}
