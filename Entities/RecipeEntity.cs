65aecad255e03d765347bf9127be62b3d5626e84
Rsda23 <rsda23.0@gmail.com>
2025-09-26 11:43:36 +0200

    feat(recipe): add properties to model entity

--- Entities/RecipeEntity.cs
+++ Entities/RecipeEntity.cs
@@ -12,32 +12,74 @@ namespace PlanMyMeal.Api.Entities
         public string RecipeId { get; set; }
 
         [BsonElement]
-        public string UserId { get; set; }
+        public string UserId { get; set; } = string.Empty;
 
         [BsonElement]
         public string Title { get; set; }
 
         [BsonElement]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [BsonElement]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement]
+        public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement]
+        public string Difficulty { get; set; }
+
+        [BsonElement]
+        public bool IsFavorite { get; set; }
+
+        [BsonElement]
+        public int TotalTimeMinutes { get; set; }
+
+        [BsonElement]
+        public string Type { get; set; }
+
+        [BsonElement]
+        public int Calories { get; set; }
+
+        [BsonElement]
+        public List<string> Diets { get; set; } = new();
+
+        [BsonElement]
+        public List<string> Tags { get; set; } = new();
+
+        [BsonElement]
         public List<IngredientEntity> Ingredients { get; set; } = new List<IngredientEntity>();
+
 
         public RecipeEntity()
         {
 
         }
 
-        public RecipeEntity(string userId, string title, List<IngredientEntity> ingredients)
+        public RecipeEntity(string title, string difficulty, string type)
         {
-            UserId = userId;
             Title = title;
-            Ingredients = ingredients;
+            Difficulty = difficulty;
+            Type = type;
         }
 
         public Recipe MapToDomain()
         {
             Recipe result = new Recipe
             {
+                RecipeId = RecipeId,
                 UserId = UserId,
                 Title = Title,
+                ImageUrl = ImageUrl,
+                CreatedAt = CreatedAt,
+                UpdateAt = UpdateAt,
+                Difficulty = Difficulty,
+                IsFavorite = IsFavorite,
+                TotalTimeMinutes = TotalTimeMinutes,
+                Type = Type,
+                Calories = Calories,
+                Diets = Diets,
+                Tags = Tags,
                 Ingredients = MapIngredients(),
             };
 
